b3a351dcfc21d3b6ccd8cc56e4ffddcc9265feec
Rostislav Szkandera <szkandera.r@seznam.cz>
2024-03-08 18:02:46 +0100

    Add space between text Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = ({onEmailClick}) => {
   return (
     <footer>
        
-        <div className="flex flex-col justify-center items-center sm:flex-row"> 
+        <div className="flex flex-col justify-center items-center sm:flex-row xl:space-x-11"> 
             <p className="mb-4 sm:mb-2 sm:mr-20">Rostislav Szkandera&#169;2024</p>
             <a className="mb-2 sm:mr-4 text-gray-500 sm:hover:text-gray-700" href="https://github.com/RostislavSzkandera/" target="_blank" rel="noopener noreferrer"><FaGithub size={25} className="" /></a>
             <a className="mb-2 sm:mr-4 text-gray-500 sm:hover:text-gray-700" href="https://www.linkedin.com/in/rostislav-szkandera-0957a02b8/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn size={25} className="" /></a>
